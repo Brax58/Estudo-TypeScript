107ff79e94071d56d2966f351421c54e740d81b3
GustavoDev <77007375+Brax58@users.noreply.github.com>
2021-12-02 23:42:47 -0300

    kk

---
+++ typescript-curso-1/app/models/negociacao.ts
@@ -0,0 +1,25 @@
+export class Negociacao
+{
+    private Data: Date;
+    private Quantidade: number;
+    private Valor : number;
+
+    get valor() { return this.Valor; }
+
+    get quantidade() { return this.Quantidade; }
+
+    get data(){ return this.Data; }
+
+    constructor(data: Date,quantidade: number,valor: number)
+    {   
+        this.Data = data;
+        this.Quantidade = quantidade;
+        this.Valor = valor;
+    }
+
+    public setQuantidade(quantidade: number) : {
+
+
+    }
+
+}
